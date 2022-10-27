83d7491a6a444b01ea770334adfac7b19710bbfc
abhaypsamrat <abhay.p.samrat@gmail.com>
2022-10-27 11:04:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import axios from "axios";
+import "./App.css";
+
+class App extends React.Component {
+  state = { advice: "" };
+
+  render() {
+    return <p>hello</p>;
+  }
+}
+export default App;
